64c57f5917bc577b1554de08699d049568c6940f
zyh001 <zyh001@users.noreply.github.com>
2026-02-14 23:40:24 +0800

    更新版本到0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="med-exam-toolkit",
-    version="0.0.2",
+    version="0.0.3",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     python_requires=">=3.10",
