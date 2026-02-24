2061e11ef708bcf7a4a2adb3e9d92293c0ac8974
zyh001 <stackzhao@gmail.com>
2026-02-24 12:52:44 +0800

    chore(version): 更新版本号从 0.0.4-R260222 到 0.1.0-R260224

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="med-exam-toolkit",
-    version="0.0.4-R260222",
+    version="0.1.0-R260224",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     python_requires=">=3.10",
