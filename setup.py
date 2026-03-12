6c9be70b601550cd2593f167545c57ee24d3ce96
zyh001 <zyh001@users.noreply.github.com>
2026-03-12 20:00:14 +0800

    chore(release): 更新版本号到 0.1.7-R260312

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="med-exam-toolkit",
-    version="0.1.7-R260310",
+    version="0.1.7-R260312",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     python_requires=">=3.10",
